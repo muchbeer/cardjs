166b735536ec545be459d55c19e7834825c92d03
George Machibya <gman2them@gmail.com>
2024-01-13 17:53:56 +0300

    react card initial stage

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+    return <div>
+    <h1 className="heading">My Contacts</h1>
+        <Card   
+        name= {contacts[0].name}
+        img = {contacts[0].imgURL}
+        tel = {contacts[0].phone}
+        email = { contacts[0].email}
+        />
+        
+    </div>
+}
+
+export default App;
