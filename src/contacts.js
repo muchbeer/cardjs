e797aa90ba2337d553d5cc69f6d697a8b9cbe34f
George Machibya <gman2them@gmail.com>
2024-01-14 20:54:44 +0300

    complete a good isolation of concern in a list

--- src/contacts.js
+++ src/contacts.js
@@ -1,5 +1,6 @@
 const contacts = [
     {
+        id : 1,
       name: "Gis Gadiel",
       imgURL:
         "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
@@ -7,6 +8,7 @@ const contacts = [
       email: "giana@gmail.com"
     },
     {
+        id : 2,
       name: "Sospter Paul",
       imgURL:
         "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
@@ -14,7 +16,8 @@ const contacts = [
       email: "jack@gmail.com"
     },
     {
-      name: "Emmanuel Kamala",
+        id : 3,
+      name: "Ema Kamala",
       imgURL:
         "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
       phone: "+918 372 574",
